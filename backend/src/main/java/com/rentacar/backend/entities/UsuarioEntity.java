8fecf0e388c99556bafb45e4939aaa0ad80a5367
felipec03 <felipe.cubillos.a@usach.cl>
2024-12-04 17:42:19 -0300

    Inicio de entidad usuario.

---
+++ backend/src/main/java/com/rentacar/backend/entities/UsuarioEntity.java
@@ -0,0 +1,33 @@
+package com.rentacar.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UsuarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    // RUT asociado al usuario.
+    private String usuarioRUT;
+
+    // Nombre en pantalla del usuario
+    private int usuarioName;
+
+    // O es Cliente, Trabajador o Administrador.
+    private String usuarioAccountType;
+
+    // Aweonao
+    private boolean usuarioIsBlacklisted;
+}
