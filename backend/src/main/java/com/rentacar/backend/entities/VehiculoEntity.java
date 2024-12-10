c6d867a25eec0dd5e6eeafcc6b8bf74830866947
solarc3 <ignacio.solar@usach.cl>
2024-12-10 11:33:19 -0300

    Arreglando branch equivocado, configurado bien el setup para empezar, mejor usar create-drop, Related to #7

--- backend/src/main/java/com/rentacar/backend/entities/VehiculoEntity.java
+++ backend/src/main/java/com/rentacar/backend/entities/VehiculoEntity.java
@@ -14,34 +14,34 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VehiculoEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true, nullable = false)
-	private Long id;
-	@Column(nullable = false)
-	private String marca;
-	@Column(nullable = false)
-	private String modelo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String marca;
+    @Column(nullable = false)
+    private String modelo;
 
-	@Column(unique = true, nullable = false, length = 6)
-	private String patente;
+    @Column(unique = true, nullable = false, length = 6)
+    private String patente;
 
-	// fab year
-	private int anio;
+    // fab year
+    private int anio;
 
-	@Column(name = "precio_arriendo", nullable = false)
-	private BigDecimal precioArriendo;
+    @Column(name = "precio_arriendo", nullable = false)
+    private BigDecimal precioArriendo;
 
-	@ManyToOne
-	@JoinColumn(name = "sucursal_id")
-	private SucursalEntity sucursal;
+    @ManyToOne
+    @JoinColumn(name = "sucursal_id")
+    private SucursalEntity sucursal;
 
-	// Disponibilidad
-	private boolean disponible;
+    // Disponibilidad
+    private boolean disponible;
 
-	// Estado del vehiculo
-	private String estado;//DISPONIBLE, NO_DISPONIBLE, EN_MANTENCION, EN_REPARACION
+    // Estado del vehiculo
+    private String estado;//DISPONIBLE, NO_DISPONIBLE, EN_MANTENCION, EN_REPARACION
 
-	@OneToMany(mappedBy = "vehiculo", cascade = CascadeType.ALL)
-	private List<ValoracionEntity> valoraciones;
+    @OneToMany(mappedBy = "vehiculo", cascade = CascadeType.ALL)
+    private List<ValoracionEntity> valoraciones;
 }
