af8a5098e9a375d1bacce7ad366d4a33ac88ae41
solarc3 <ignacio.solar@usach.cl>
2024-12-10 15:49:19 -0300

    Agregado el componente de vehiculo, falta agregar mas cosas, esperando el back. Related to #4.

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -19,22 +19,63 @@
       <RegisterMenu />
     </v-dialog>
 
-    <v-main>
+    <v-main style="background-color: white;">
       <Buscador />
-      <!-- aqui va todo lo de los autos abajo!!! -->}
+      <v-container
+        width="100%"
+        min-width="100%"
+      >
+        <div class="text-center">
+          <v-pagination
+            v-model="currentPage"
+            :length="numberOfPages"
+            :total-visible="7"
+          />
+        </div>
+        <v-row style="padding: 10px">
+          <v-col
+            v-for="n in displayedItems"
+            :key="n"
+            cols="12"
+            sm="6"
+            md="2"
+            lg="3"
+            style="padding: 10px"
+          >
+            <VehiculoCard />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 import AppBar from "@/components/AppBar.vue";
 import LoginMenu from "@/components/Login.vue";
 import RegisterMenu from "@/components/Register.vue";
 import Buscador from "@/components/Buscador.vue";
+import VehiculoCard from "@/components/VehiculoCard.vue";
 
 const showLoginMenu = ref(false);
 const showRegisterMenu = ref(false);
+
+//paginacion
+const itemsPerPage = 12;
+const totalItems = 48;
+const currentPage = ref(1);
+
+
+const numberOfPages = computed(() => {
+  return Math.ceil(totalItems / itemsPerPage);
+});
+
+const displayedItems = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  const end = Math.min(start + itemsPerPage, totalItems);
+  return end - start;
+});
 
 const showLoginForm = () => {
   showLoginMenu.value = true;
