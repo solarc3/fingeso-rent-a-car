ffa7436db5f8639158ce34115fafdf32cf540c9a
nakssu <josepha.gaete@usach.cl>
2024-12-16 18:18:31 -0300

    Lo del About.vue

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,19 +1,65 @@
 <template>
-  <v-container>
-    <h1> Sobre Nosotros! </h1>
+  <!-- Contenido Principal -->
+  <v-container
+    fluid
+    class="pa-0"
+  >
+    <v-row
+      no-gutters
+      style="min-height: 100vh;"
+    >
+      <v-col
+        cols="12"
+        md="6"
+        class="d-flex align-center justify-center pa-10"
+        style="background-color: #d1e4ff"
+      >
+        <div>
+          <h1
+            class="text-h2 font-weight-bold"
+            style="color: #333;"
+          >
+            Sobre nosotros
+          </h1>
+          <p
+            class="text-subtitle-1 mt-4"
+            style="color: #555;"
+          >
+            Somos una...
+          </p>
+        </div>
+      </v-col>
 
-    <p> Página web bacán. </p>
+      <v-img
+        src="../assets/about.png"
+        max-width="800"
+        class="mx-auto mb-4"
+      />
+    </v-row>
 
-    <h2> Objetivos cómo empresa: </h2>
-
-    <p> Yap... </p>
+    <v-footer
+      color="#002349"
+      padless
+    />
   </v-container>
 </template>
 
 <script>
-export default {
 
-};
 </script>
+
+<style scoped>
+h1 {
+  line-height: 1.2;
+}
+
+
+p {
+  font-size: 1.1rem;
+  line-height: 1.8;
+}
+
+</style>
+
 
 
